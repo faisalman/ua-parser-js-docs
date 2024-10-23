c9561e1777a782001e99993ae4cfe09587ab4ef5
faisalman <faisalman@users.noreply.github.com>
2024-10-23 01:49:04 +0000

    deploy: 2422d72c9565eb2be10c6cac046475cebde82fb6

--- docs/assets/info_os_name.md.f66b0c3f.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as i,N as c}from"./chunks/framework.2ea5d489.js";const O=JSON.parse('{"title":"os.name","description":"","frontmatter":{},"headers":[],"relativePath":"info/os/name.md","lastUpdated":1728740851000}'),d={name:"info/os/name.md"},l=c("",3),a=[l];function n(t,r,s,S,m,_){return i(),o("div",null,a)}const h=e(d,[["render",n]]);export{O as __pageData,h as default};
