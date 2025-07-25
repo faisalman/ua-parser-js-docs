225bec6bc3eb8e55a4c3f99dffe5fb1dc248a9e7
Faisal Salman <f@faisalman.com>
2025-07-25 10:28:40 +0700

    Update changelog

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -27,7 +27,7 @@ export default defineConfig({
         nav: [
             { text: 'Home', link: '/' },
             {
-                text: '2.0.3',
+                text: '2.0.4',
                 items: [
                     {
                         text: 'Changelog',
